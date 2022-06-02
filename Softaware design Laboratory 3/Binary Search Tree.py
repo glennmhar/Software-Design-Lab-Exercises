3e7a82eb8bcbb8707266c3add942119ffa3753a8
glennmhar <106755069+glennmhar@users.noreply.github.com>
2022-06-03 04:38:26 +0800

    Add files via upload

---
+++ Softaware design Laboratory 3/Binary Search Tree.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.val = item
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(str(root.val) + "->", end='')
+        inorder(root.right)
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(str(root.val) + "->", end='')
+def preorder(root):
+    if root:
+        print(str(root.val) + "->", end='')
+        preorder(root.left)
+        preorder(root.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+def count_nodes(root):
+    if root is None:
+        return 0
+    return 1 + count_nodes(root.left) + count_nodes(root.right)
+
+print("Inorder traversal ")
+inorder(root)
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
+
+print("\nNumber of nodes in tree:")
+print (count_nodes(root))
